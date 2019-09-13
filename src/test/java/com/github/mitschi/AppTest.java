f621f5d2aa6406cebb0b719c904592cf231c73d3
Christian Macho <christian.macho@gmx.at>
2019-09-13 20:21:21 +0200

    test commit

--- src/test/java/com/github/mitschi/AppTest.java
+++ src/test/java/com/github/mitschi/AppTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 /**
  * Unit test for simple HelloController.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        assertTrue(true);
     }
 }
